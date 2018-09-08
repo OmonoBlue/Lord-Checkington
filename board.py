dd03fa5d5877ee431215ea760e7820dd097b4ec7
OmonoBlue <43080711+OmonoBlue@users.noreply.github.com>
2018-09-07 20:27:35 -0400

    Add files via upload

--- board.py
+++ board.py
@@ -3,14 +3,14 @@ import math
 
 class NewBoard():
 
-    def __init__(self, length, height):
+    def __init__(self, width, height):
 
-        array = []
+        pos = [[None for x in range(width)] for y in range(height)]
 
-        for row in range(height):
-            print row
-
-
+        for x in range(len(pos)):
+            for y in range(len(pos[x])):
+                print pos[x][y]
+        
 
 board = NewBoard(8, 8)
 
