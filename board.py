fda8a1b6e549d554e1350272420e585323b167ff
OmonoBlue <43080711+OmonoBlue@users.noreply.github.com>
2018-09-07 20:10:10 -0400

    Add files via upload

---
+++ board.py
@@ -0,0 +1,16 @@
+import math
+
+
+class NewBoard():
+
+    def __init__(self, length, height):
+
+        array = []
+
+        for row in range(height):
+            print row
+
+
+
+board = NewBoard(8, 8)
+
