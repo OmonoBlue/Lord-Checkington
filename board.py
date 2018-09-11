ba60332a5a8fd6f22c9f6927d23a424d11a46f64
OmonoBlue <43080711+OmonoBlue@users.noreply.github.com>
2018-09-10 21:56:30 -0400

    Add files via upload

--- board.py
+++ board.py
@@ -15,6 +15,8 @@ class NewBoard():
 
         self.width = width
         self.height = height
+
+        self.turn = 0
         # Setup pieces
 
         # total pieces remaining for each color
@@ -31,7 +33,7 @@ class NewBoard():
                 if space % 2 != mode:
 
                     #pieces are set up like this: self.pos[row][space](color {0 = white / 1 = black}, king {True/False})
-                    self.pos[row][space] = (0, False)
+                    self.pos[row][space] = [0, False]
                     totalw -= 1
 
 
@@ -53,46 +55,46 @@ class NewBoard():
             if totalb < 1:
                 break
 
-    def Move(self, origin, dr, ds): #dr stands for direction row, ds stands for direction space (y and x respectively)
-        if abs(dr) != 1 or abs(ds) != 1:
+    def Move(self, originY, originX, dirY, dirX): #dirX stands for direction row, dirY stands for direction space (y and x respectively)
+        if abs(dirX) != 1 or abs(dirY) != 1:
             print "Can only move 1 space in each direction!"
-        dest = (origin[0] + dr,origin[1] + ds)
+        dest = (originX + dirX,originY + dirY)
         print dest, "is the destination"
         
-        print self.pos[origin[0]][origin[1]], "has been selected"
+        print self.pos[originX][originY], "has been selected"
         
-        if self.pos[origin[0]][origin[1]] != None: #Check if a playable piece has been selected
+        if self.pos[originX][originY] != None: #Check if a playable piece has been selected
             print "piece detected"
             
             #Check if playable piece is moveable in chosen direction
-            if (self.pos[origin[0]][origin[1]][P_COL] == 0 and dr == 1) or (self.pos[origin[0]][origin[1]][P_COL] == 1 and dr == -1) or self.pos[origin[0]][origin[1]][P_KING] == True:
+            if (self.pos[originX][originY][P_COL] == 0 and dirX == 1) or (self.pos[originX][originY][P_COL] == 1 and dirX == -1) or self.pos[originX][originY][P_KING] == True:
                 
                 if (dest[0] > self.width - 1 or dest[0] < 0) or (dest[1] > self.height - 1 or dest[1] < 0): #check if the piece is going to move out of bounds
                     print "can't move, piece out of bounds"
                 elif self.pos[dest[0]][dest[1]] != None: #if the destination is not clear...
                     print "destination blocked..."
-                    if self.pos[dest[0]][dest[1]][P_COL] == self.pos[origin[0]][origin[1]][P_COL]: #check if the space is occupied by your own colour
+                    if self.pos[dest[0]][dest[1]][P_COL] == self.pos[originX][originY][P_COL]: #check if the space is occupied by your own colour
                         print "can't move, piece occupied by own colour"
                     else: #If piece is enemy color...
-                        if self.pos[dest[0] + dr][dest[1] + ds] != None: #check if piece is capturable
+                        if self.pos[dest[0] + dirX][dest[1] + dirY] != None: #check if piece is capturable
                             print "can't move, capture blocked"
                         else:
                             #Capture piece
                             print "capturing..."
-                            self.pos[dest[0] + dr][dest[1] + ds] = self.pos[origin[0]][origin[1]]
+                            self.pos[dest[0] + dirX][dest[1] + dirY] = self.pos[originX][originY]
                             self.pos[dest[0]][dest[1]] = None
-                            self.pos[origin[0]][origin[1]] = None
-                            if ((dest[0] + dr == self.height - 1) and self.pos[dest[0] + dr][dest[1] + ds][P_COL] == 0) or ((dest[0] + dr == 0) and self.pos[dest[0] + dr][dest[1] + ds][P_COL] == 1):
+                            self.pos[originX][originY] = None
+                            if ((dest[0] + dirX == self.height - 1) and self.pos[dest[0] + dirX][dest[1] + dirY][P_COL] == 0) or ((dest[0] + dirX == 0) and self.pos[dest[0] + dirX][dest[1] + dirY][P_COL] == 1):
                                 print "King me!"
-                                self.pos[dest[0] + dX][dest[1] + dY][P_KING] = True
+                                self.pos[dest[0] + dirX][dest[1] + dirY][P_KING] = True
                 else:
                     print "moving..."
-                    self.pos[dest[0]][dest[1]] = self.pos[origin[0]][origin[1]]
-                    self.pos[origin[0]][origin[1]] = None
+                    self.pos[dest[0]][dest[1]] = self.pos[originX][originY]
+                    self.pos[originX][originY] = None
                     #Move piece
-                    if ((dest[0] == self.height - 1) and self.pos[dest[0]][dest[1]][P_COL] == 0) or ((dest[0] == 0) and self.pos[dest[0] + dr][dest[1] + ds][P_COL] == 1):
+                    if ((dest[0] == self.height - 1) and self.pos[dest[0]][dest[1]][P_COL] == 0) or ((dest[0] == 0) and self.pos[dest[0] + dirX][dest[1] + dirY][P_COL] == 1):
                         print "King me!"
-                        self.pos[dest[0] + dX][dest[1] + dY][P_KING] = True
+                        self.pos[dest[0] + dirX][dest[1] + dirY][P_KING] = True
             else:
                 print "You can't move in that direction!"
         else:
@@ -111,7 +113,7 @@ class NewBoard():
                     else:
                         print "X",
                 else:
-                    print "Â·",
+                    print "·",
             print "\n",
             if row == 0:
                 print "|",
@@ -119,8 +121,15 @@ class NewBoard():
                     print space,
                 print "\n"
 
-    def GetStats(self):
+          
+    def GetStats(self, flipped):
         statList = []
+        
+        if flipped == True:
+            order = -1
+        else:
+            order = 1
+            
         for row in range(len(self.pos)):
             for space in range(len(self.pos[row])):
                 
@@ -132,16 +141,17 @@ class NewBoard():
                         statList.append(0)
                         
                     else:
+
                         if self.pos[row][space][P_COL] == 0:
                             if self.pos[row][space][P_KING] == True:
-                                statList.append(-3)
+                                statList.append(-3 * order)
                             else:
-                                statList.append(-1)
+                                statList.append(-1 * order)
                         else:
                             if self.pos[row][space][P_KING] == True:
-                                statList.append(3)
+                                statList.append(3 * order)
                             else:
-                                statList.append(1)
+                                statList.append(1 * order)
         return statList
                             
 
