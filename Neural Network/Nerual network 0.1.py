6e875259b31f5d561716f7e7f49726d35cf067c1
WoaZ1 <43080841+WoaZ1@users.noreply.github.com>
2018-11-06 12:15:55 -0500

    Add files via upload

--- Neural Network/Nerual network 0.1.py
+++ Neural Network/Nerual network 0.1.py
@@ -7,11 +7,31 @@ from Functions import Weight_Puller, maths, NetworkRecorder, dataSetOrganizer, c
 def neuralNetwork(values):
     weights = []
     nodes = []
+    
 
-    (weights, nodes) = Weight_Puller.weightPuller()
+    (weights, nodes, games) = Weight_Puller.weightPuller()
+    nodesList = [None] * len(nodes)
+    
 
-    print weights
-    print nodes
+    nodesList[0] = values
+    for i in range(1,len(nodes)):
+        nodesList[i] = [None] * nodes[i]
+        for a in range(nodes[i]):
+            nodesList[i][a] = 0
+    for layer in range (len(nodes)-1):
+        for node in range (len(nodesList[layer])) :
+            for i in range(len(weights[layer][node])):
+                nodesList[layer+1][i] += (float(nodesList[layer][node]) * float(weights[layer][node][i]))
 
+    for i in range (len(nodesList[len(nodes)-1])):
+        nodesList[len(nodes)-1][i] = maths.sigmoid(nodesList[len(nodes)-1][i])
+                
 
-neuralNetwork(1)
+        
+    print nodesList[3]
+    print len(nodesList[3])
+                               
+
+list1 = [1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+
+neuralNetwork(list1)
